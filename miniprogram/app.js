4df71eae79d7d54c4bbebd076c158df1967fd054
刘朝辉 <78063048+LIU20020403@users.noreply.github.com>
2022-10-14 16:33:58 +0800

    test
    no

--- miniprogram/app.js
+++
@@ -1,9 +0,0 @@
-// app.js
-App({
-  onLaunch : function () {
-     wx.cloud.init({
-      env:"cloud1-9goigp37ed469d6c",
-       traceUser:true
-     })
-  },
-});
--- miniprogram/envList.js
+++
@@ -1,6 +0,0 @@
-const envList = [{"envId":"cloud1-9goigp37ed469d6c","alias":"cloud1"}]
-const isMac = true
-module.exports = {
-    envList,
-    isMac
-}
